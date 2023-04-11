ffdd3593d621f24709931627e015249609ac1349
kanywst <niwatakuma@icloud.com>
2023-04-11 22:26:41 +0900

    update README.md

--- cmd/x509.go
+++ cmd/x509.go
@@ -19,10 +19,9 @@ var (
 	o = &Options{}
 )
 
-// x509Cmd represents the x509 command
 var x509Cmd = &cobra.Command{
 	Use:   "x509",
-	Short: "display x509 certificate",
+	Short: "display necessary information about x509 certificates",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Print(certificate.GetX509Information(o.certificateFileName, o.issuerCommonName, o.subjectCommonName, o.validity, o.dns))
 	},
@@ -31,10 +30,10 @@ var x509Cmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(x509Cmd)
 
-	x509Cmd.Flags().StringVarP(&o.certificateFileName, "file", "f", "", "Certificate input file")
-	x509Cmd.Flags().BoolVarP(&o.issuerCommonName, "issuer", "i", false, "Display issuer")
-	x509Cmd.Flags().BoolVarP(&o.subjectCommonName, "subject", "s", false, "Display subject")
-	x509Cmd.Flags().BoolVarP(&o.validity, "validity", "v", false, "Display validity")
-	x509Cmd.Flags().BoolVarP(&o.dns, "dns", "d", false, "Display X509v3 Subject Alternative Name")
+	x509Cmd.Flags().StringVarP(&o.certificateFileName, "pem", "p", "", "specify certificate input pem")
+	x509Cmd.Flags().BoolVarP(&o.issuerCommonName, "issuer", "i", false, "display issuer")
+	x509Cmd.Flags().BoolVarP(&o.subjectCommonName, "subject", "s", false, "display subject")
+	x509Cmd.Flags().BoolVarP(&o.validity, "validity", "v", false, "display validity")
+	x509Cmd.Flags().BoolVarP(&o.dns, "dns", "d", false, "display subject alternative name")
 
 }
