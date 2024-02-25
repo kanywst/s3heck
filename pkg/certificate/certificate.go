d4af9efa8dbad3741ed20e2564a25b90c164eba1
kt <45947799+kanywst@users.noreply.github.com>
2024-02-25 20:49:39 +0900

    v1.0.1 (#12)

--- pkg/certificate/certificate.go
+++ pkg/certificate/certificate.go
@@ -4,59 +4,76 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
-	"io/ioutil"
+	"os"
 	"strings"
 
 	"github.com/kanywst/s3heck/pkg/certinfo"
 )
 
-func GetX509Information(inputCertificate string, issuer bool, subject bool, validity bool, dns bool, short bool) (d string) {
-	var (
-		chain     [][]byte
-		certBlock *pem.Block
-	)
+func GetX509Information(inputCertificate string, issuer, subject, validity, dns, short bool) string {
+	file, err := os.Open(inputCertificate)
+	if err != nil {
+		return fmt.Sprintf("Error opening certificate file: %v", err)
+	}
+	defer file.Close()
 
-	certBytes, _ := ioutil.ReadFile(inputCertificate)
+	stat, err := file.Stat()
+	if err != nil {
+		return fmt.Sprintf("Error getting file information: %v", err)
+	}
+
+	certBytes := make([]byte, stat.Size())
+	_, err = file.Read(certBytes)
+	if err != nil {
+		return fmt.Sprintf("Error reading certificate file: %v", err)
+	}
+
+	var chain [][]byte
 	for {
-		certBlock, certBytes = pem.Decode(certBytes)
+		certBlock, rest := pem.Decode(certBytes)
 		if certBlock == nil {
 			break
 		}
 		if certBlock.Type == "CERTIFICATE" {
 			chain = append(chain, certBlock.Bytes)
 		}
+		certBytes = rest
 	}
+
+	var result strings.Builder
 	for i, c := range chain {
-		cert, _ := x509.ParseCertificate(c)
-		s := fmt.Sprintf("Certificate: SerialNumber is %x\n", cert.SerialNumber)
-		d += s
+		cert, err := x509.ParseCertificate(c)
+		if err != nil {
+			result.WriteString(fmt.Sprintf("Error parsing certificate: %v\n", err))
+			continue
+		}
+
+		result.WriteString(fmt.Sprintf("Certificate: SerialNumber is %x\n", cert.SerialNumber))
 		if short {
-			sanDns := strings.Join(cert.DNSNames, ", ")
-			d += certinfo.PrintFgColor("Issuer", cert.Issuer.CommonName, certinfo.FgColors[i%len(certinfo.FgColors)+1])
-			if len(sanDns) != 0 {
-				d += certinfo.PrintFgColor("Subject", cert.Subject.CommonName+", "+strings.Join(cert.DNSNames, ", "), certinfo.FgColors[i%len(certinfo.FgColors)])
-			} else {
-				d += certinfo.PrintFgColor("Subject", cert.Subject.CommonName, certinfo.FgColors[i%len(certinfo.FgColors)])
+			issuerName := cert.Issuer.CommonName
+			subjectName := cert.Subject.CommonName
+			if len(cert.DNSNames) > 0 {
+				subjectName += ", " + strings.Join(cert.DNSNames, ", ")
 			}
-			d += certinfo.PrintValidity(cert.NotBefore, cert.NotAfter)
+			result.WriteString(certinfo.PrintFgColor("Issuer", issuerName, certinfo.FgColors[i%len(certinfo.FgColors)+1]))
+			result.WriteString(certinfo.PrintFgColor("Subject", subjectName, certinfo.FgColors[i%len(certinfo.FgColors)]))
+			result.WriteString(certinfo.PrintValidity(cert.NotBefore, cert.NotAfter))
 		} else {
 			if issuer {
-				d += certinfo.PrintFgColor("Issuer", cert.Issuer.CommonName, certinfo.FgColors[i%len(certinfo.FgColors)+1])
+				result.WriteString(certinfo.PrintFgColor("Issuer", cert.Issuer.CommonName, certinfo.FgColors[i%len(certinfo.FgColors)+1]))
 			}
 			if subject {
-				d += certinfo.PrintFgColor("Subject", cert.Subject.CommonName, certinfo.FgColors[i%len(certinfo.FgColors)])
+				result.WriteString(certinfo.PrintFgColor("Subject", cert.Subject.CommonName, certinfo.FgColors[i%len(certinfo.FgColors)]))
 			}
 			if validity {
-				d += certinfo.PrintValidity(cert.NotBefore, cert.NotAfter)
+				result.WriteString(certinfo.PrintValidity(cert.NotBefore, cert.NotAfter))
 			}
 			if dns {
-				d += certinfo.PrintX509Extensions(strings.Join(cert.DNSNames, ", "))
+				result.WriteString(certinfo.PrintX509Extensions(strings.Join(cert.DNSNames, ", ")))
 			}
 		}
-		for i := 0; i < len(s)-1; i++ {
-			d += "-"
-		}
-		d += "\n"
+		result.WriteString(strings.Repeat("-", result.Len()-1) + "\n")
 	}
-	return
+
+	return result.String()
 }
